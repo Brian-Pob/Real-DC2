c12e6082497d24b727ff8ec70601c4561478625e
Unknown <adriennesoliven@yahoo.com>
2018-03-07 00:38:41 +0800

    Made model
    it has import and export methods but import doesnt work bec there's no view to connect it with

---
+++ src/designchallenge2/Model.java
@@ -0,0 +1,26 @@
+package designchallenge2;
+
+import java.util.ArrayList;
+
+public class Model {
+	private DataParserCSV dp;
+	private DataWriterCSV dw;
+	
+	public void importOccasions(){
+    
+        
+        try {
+			dp = new DataParserCSV("List of Occasions.csv");
+			//view.addAllEvents(dp.processData());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+    }
+    
+    public void exportOccasions(ArrayList<Occasion> occasions){
+
+            dw = new DataWriterCSV(occasions);
+    }
+
+}
